b8698204e869592ce3922222a27fa30d301a5093
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-13 11:21:16 +0530

    Add : Welcome Message to the Cab Invoice Generator

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CabinInvoiceGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator!");
+            
+        }
+    }
+}
