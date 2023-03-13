3b13e918dbae44e6405eb3f6f721fe01bdcc0393
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-13 11:24:45 +0530

    Add : Calculate Fare

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Cab Invoice Generator!");
-            
+            InvoiceGenerator uc1 = new();
+            double fare1 = uc1.CalculateFare(10, 30);
+            Console.WriteLine("Total Fare for the Journey is :" + fare1);
         }
     }
 }
